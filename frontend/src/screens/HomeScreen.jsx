85ebdc451e2dbebe1bbf9dfc4b7f78e173bd3631
Aja-Edward <77103145+Aja-Edward@users.noreply.github.com>
2023-12-28 11:26:20 +0100

    improved redux

--- frontend/src/screens/HomeScreen.jsx
+++ frontend/src/screens/HomeScreen.jsx
@@ -1,31 +1,53 @@
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 import { Row, Col } from 'react-bootstrap'
 // import products from '../products'
 import Product from '../components/Product'
-import axios from 'axios'
+import { useGetProductsQuery } from '../slices/productsApiSlice'
+// import axios from 'axios'
 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
+  const { data: products, isLoading, error } = useGetProductsQuery()
+  // const [products, setProducts] = useState([])
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products')
-      console.log(data)
+  // useEffect(() => {
+  //   const fetchProducts = async () => {
+  //     const { data } = await axios.get('/api/products')
+  //     console.log(data)
 
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
+  //     setProducts(data)
+  //   }
+  //   fetchProducts()
+  // }, [])
+  // return (
+  //   <>
+  //     <h1>Latest Products</h1>
+  //     <Row>
+  //       {products.map((product) => (
+  //         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+  //           <Product product={product} />
+  //         </Col>
+  //       ))}
+  //     </Row>
+  //   </>
+  // )
   return (
     <>
-      <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product} />
-          </Col>
-        ))}
-      </Row>
+      {isLoading ? (
+        <h2>Loading ...</h2>
+      ) : error ? (
+        <div>{error.data?.message || error.error}</div>
+      ) : (
+        <>
+          <h1>Latest Products</h1>
+          <Row>
+            {products.map((product) => (
+              <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                <Product product={product} />
+              </Col>
+            ))}
+          </Row>
+        </>
+      )}
     </>
   )
 }
